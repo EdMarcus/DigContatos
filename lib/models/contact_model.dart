44649101be9aafe80af1f52ef5dad75c4b7e6ead
Édlon de Sá <edlon2007@gmail.com>
2020-06-04 23:24:47 -0300

    Finish cardContact layourt.

---
+++ lib/models/contact_model.dart
@@ -0,0 +1,9 @@
+class Contact {
+  final String name;
+  final String email;
+  final String phone;
+  final String urlPhoto;
+
+  Contact({this.name, this.email, this.phone, this.urlPhoto});
+
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -16,7 +16,6 @@ class _HomeViewState extends State<HomeView> {
       ),
       child: Container(
         width: double.maxFinite,
-        height: 200.0,
         decoration: BoxDecoration(
             gradient: LinearGradient(
           colors: [Colors.blue, Colors.indigo],
@@ -24,7 +23,7 @@ class _HomeViewState extends State<HomeView> {
           end: Alignment.bottomCenter,
         )),
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+          padding: const EdgeInsets.fromLTRB(10, 10, 0, 20),
           child: Column(
             children: <Widget>[
               Row(
@@ -36,34 +35,97 @@ class _HomeViewState extends State<HomeView> {
                       backgroundColor: Colors.grey[50],
                       child: Icon(
                         Icons.person,
-                        size: 40,
+                        size: 50,
                         color: Colors.blueAccent,
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 4,
-                    child: Container(
-                        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: Text("Édlon de Sá")),
+                  Container(
+                    child: Expanded(
+                      flex: 4,
+                      child: Container(
+                        margin: EdgeInsets.only(right: 10),
+                          padding: EdgeInsets.fromLTRB(15, 10, 5, 20),
+                          child: Text(
+                            "Édlon Marcus M. de Sá",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                    ),
                   ),
                   Expanded(
                     flex: 1,
                     child: IconButton(
+                      alignment: Alignment.topCenter,
                       icon: Icon(
                         Icons.edit,
                         color: Colors.grey[50],
-                        ),
+                      ),
                       onPressed: () {},
                     ),
                   )
                 ],
               ),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("teste"),
-                  Text("teste"),
-                  Text("teste")
+                  Expanded(
+                    flex: 1,
+                    child: SizedBox(
+                      width: double.infinity,
+                    ),
+                  ),
+                  Expanded(
+                      flex: 5,
+                      child: Container(
+                          padding: EdgeInsets.fromLTRB(5, 10, 20, 0),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.mail,
+                                    size: 20,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    "edlon.sa@aluno.ufop.br",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 5,),
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.phone,
+                                    size: 20,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    "(31) 9 9938-4640",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ))),
+                 
                 ],
               )
             ],
@@ -81,16 +143,18 @@ class _HomeViewState extends State<HomeView> {
             colors: [Colors.blue, Colors.indigo],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter),
-        title: Text("Contatos"),
+        title: Text("DIG CONTATOS", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.fromLTRB(15,10,15,10),
           child: Center(
             child: Column(
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
+                cardContact(),
+                cardContact(),
                 cardContact(),
                 cardContact(),
                 cardContact(),
